0a90482793fc4d6355ac9122ae9b7ba073e20057
Carl A Palumbo <carlpalumbo1@gmail.com>
2020-09-08 16:03:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App'
 import manageUsers from './reducers/manageUsers'
 
 
+let store = createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
 
 ReactDOM.render(
-  // add imports and code
+  <Provider store={store}>
   <App />
+  </Provider>
   // add imports and code
   ,
   document.getElementById('root')
